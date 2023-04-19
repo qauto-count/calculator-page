afc82311532a9d78a260bb61a48b92495626446f
qauto-count <127936744+qauto-count@users.noreply.github.com>
2023-04-19 21:43:12 +0300

    Add files via upload

--- js_data/canada.js
+++ js_data/canada.js
@@ -1,1 +1,97 @@
 
+let CANADA = {
+    auction: {
+        IMPACT: {
+            location: {
+                Montreal_Toronto: {
+                    value_0: ['LONDON', 825],
+                    value_1: ['HAMILTON', 775],
+                    value_2: ['TORONTO /OSHAWA', 750],
+                    value_3: ['STOUFFVILLE', 775],
+                    value_4: ['OTTAWA', 625],
+                    value_5: ['MONTRÉAL', 275],
+                    value_6: ['SUDBURY', 975],
+                    value_7: ['VANCOUVER', 2150],
+                    value_8: ['CALGARY', 1850],
+                    value_9: ['EDMONTON', 1850],
+                    value_10: ['HALIFAX', 1025],
+                    value_11: ['MONCTON', 950],
+                    value_12: ['ST JOHNS (Paradise,NL A1L 2E6)', 1750],
+                    value_13: ['QUEBEC CITY', 675],
+                    value_14: ['THUNDERBAY', 1400],
+                    value_15: ['KENORA', 1450]
+                }
+            }
+        },
+        SGI_SALVAGE: {
+            location: {
+                Montreal_Toronto: {
+                    value_0: ['Offsite + TOWING', 2700],
+                    value_1: ['ONLY SGI YARDS', 1750]
+                }
+            }
+        },
+        MANITOBA_PUBLIC: {
+            location: {
+                Montreal_Toronto: {
+                    value_0: ['MANITOBA', 1650],
+                    value_1: ['MANITOBA Offsite', 0],
+                    value_2: ['BRANDON', 1800],
+                    value_3: ['Steinbach', 1700]
+                }
+            }
+        },
+        COPART: {
+            location: {
+                Montreal_Toronto: {
+                    value_0: ['WINDSOR', 1250],
+                    value_1: ['LONDON', 800],
+                    value_2: ['TORONTO', 775],
+                    value_3: ['WINCHESTER', 700],
+                    value_4: ['CALGARY', 1800],
+                    value_5: ['EDMONTON', 1800],
+                    value_6: ['HALIFAX', 1025],
+                    value_7: ['MONCTON', 925],
+                    value_8: ['SAINT PHILIBERT', 925],
+                    value_9: ['CHICOUTIMI', 1025],
+                    value_10: ['MONTRÉAL', 275],
+                    value_11: ['BONEAVENTURE', 1450]
+                } 
+            }
+        },
+        STARKS: {
+            location: {
+                Montreal_Toronto: {
+                    value_0: ['TORONTO', 800]
+                }
+            }
+        },
+        Daniellessard: {
+            location: {
+                Montreal_Toronto: {
+                    value_0: ['QUEBEC', 850]
+                }
+            }
+        },
+        ICBC: {
+            location: {
+                Montreal_Toronto: {
+                    value_0: ['New Westminster', 2400],
+                    value_1: ['ABBOTSFORDSALVAGE', 2400],
+                    value_2: ['NANAIMO SALVAGE', 2700],
+                    value_3: ['VICTORIA SALVAGE', 2700],
+                    value_4: ['VERNON SALVAGE', 2800],
+                    value_5: ['PRINCE GEORGESALVAGE', 3100],
+                    value_6: ['Prince Rupert', 3400],
+                    value_7: ['Kelowna', 3000],
+                    value_8: ['Kamplops', 3000],
+                    value_9: ['Fernnie', 3700],
+                    value_10: ['DAWSON CREEK BC', 3000],
+                    value_11: ['Chetwynd, BC', 3200],
+                    value_12: ['Terrace', 3800],
+                    value_13: ['Fort St. John', 3100]
+                }
+            }
+        }
+    }
+};
--- js_data/destination.js
+++ js_data/destination.js
@@ -1,1 +1,64 @@
 
+let destination = {
+    destination_name: ['Klaipeda,Lithuania', 
+                        'Bremerhaven, Germany', 
+                        'Rotterdam,Netherlands', 
+                        'Gdynia, Poland'],
+                        
+    location_name_USA: ['GA', 'CA', 'NY', 'TX', 'WA'],
+    location_name_CANADA: ['Montreal_Toronto'],
+    country: {
+        USA: {
+            level_1: {
+                destination_cost_0: [970, 1470, 980, 1030, 2050],
+                destination_cost_1: [850, 1570, 850, 890, 2130],
+                destination_cost_2: [860, 1440, 875, 925, 2075],
+                destination_cost_3: [970, 1590, 1090, 1030, 2080]
+            },
+            level_2: {
+                destination_cost_0: [870, 1370, 880, 930, 1940],
+                destination_cost_1: [750, 1470, 750, 790, 2030],
+                destination_cost_2: [760, 1340, 775, 825, 1975],
+                destination_cost_3: [870, 1490, 990, 930, 1980]
+            },
+            level_3: {
+                destination_cost_0: [770, 1270, 780, 830, 1840],
+                destination_cost_1: [650, 1370, 650, 690, 1930],
+                destination_cost_2: [660, 1240, 675, 725, 1875],
+                destination_cost_3: [770, 1390, 890, 830, 1880]
+            },
+            level_4: {
+                destination_cost_0: [1420, 1920, 1430, 1480, 2500],
+                destination_cost_1: [1300, 2020, 1300, 1340, 2580],
+                destination_cost_2: [1310, 1890, 1325, 1375, 2525],
+                destination_cost_3: [1420, 2040, 1540, 1480, 2530]
+            }
+        },
+        CANADA: {
+            level_1: {
+                destination_cost_0: [1500],
+                destination_cost_1: [1450],
+                destination_cost_2: [1450],
+                destination_cost_3: [1500]
+            },
+            level_2: {
+                destination_cost_0: [1350],
+                destination_cost_1: [1300],
+                destination_cost_2: [1300],
+                destination_cost_3: [1350]
+            },
+            level_3: {
+                destination_cost_0: [1250],
+                destination_cost_1: [1200],
+                destination_cost_2: [1200],
+                destination_cost_3: [1250]
+            },
+            level_4: {
+                destination_cost_0: [1900],
+                destination_cost_1: [1850],
+                destination_cost_2: [1850],
+                destination_cost_3: [1900]
+            }
+        }
+    }
+};
