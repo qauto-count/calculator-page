3ad5fb4046c5d168a17bc419b96a89ead498e98e
qauto-count <127936744+qauto-count@users.noreply.github.com>
2023-09-19 16:19:06 +0300

    Add files via upload

---
+++ destination.js
@@ -0,0 +1,123 @@
+
+let destination = {
+    destination_name: ['Klaipeda,Lithuania', 
+                       'Bremerhaven, Germany', 
+                       'Rotterdam,Netherlands', 
+                       'Gdynia, Poland'],
+                        
+    location_name_USA: ['GA', 'CA', 'NJ', 'TX', 'WA', 'IN'],
+    location_name_CANADA: ['Montreal_Toronto'],
+    location_name_EUROPE: ['EUROPE'],
+    country: {
+        USA: {
+            w_1: {
+                level_1: {
+                    destination_cost_0: [1085, 1725, 1200, 1275, 2345, 0],
+                    destination_cost_1: [1100, 1800, 1100, 1175, 2345, 0],
+                    destination_cost_2: [1085, 1725, 1200, 1275, 2345, 0],
+                    destination_cost_3: [1100, 1800, 1100, 1175, 2345, 0]
+                },
+                level_2: {
+                    destination_cost_0: [1085, 1625, 1100, 1175, 2245, 0],
+                    destination_cost_1: [1000, 1700, 1000, 1075, 2245, 0],
+                    destination_cost_2: [1085, 1625, 1100, 1175, 2245, 0],
+                    destination_cost_3: [1000, 1700, 1000, 1075, 2245, 0]
+                },
+                level_3: {
+                    destination_cost_0: [1085, 1625, 1100, 1175, 2245, 0],
+                    destination_cost_1: [1000, 1700, 1000, 1075, 2245, 0],
+                    destination_cost_2: [1085, 1625, 1100, 1175, 2245, 0],
+                    destination_cost_3: [1000, 1700, 1000, 1075, 2245, 0]
+                },
+                level_4: {
+                    destination_cost_0: [1710, 2320, 1725, 1770, 2870, 0],
+                    destination_cost_1: [1600, 2320, 1600, 1650, 2870, 0],
+                    destination_cost_2: [1710, 2320, 1725, 1770, 2870, 0],
+                    destination_cost_3: [1600, 2320, 1600, 1650, 2870, 0]
+                }
+            },
+            loop_2: {
+                level_1: {
+                    destination_cost_0: [1185, 1725, 1200, 1250, 0, 1725],
+                    destination_cost_1: [1100, 1800, 1100, 1200, 0, 1800],
+                    destination_cost_2: [1185, 1725, 1200, 1250, 0, 1725],
+                    destination_cost_3: [1100, 1800, 1100, 1200, 0, 1800]
+                },
+                level_2: {
+                    destination_cost_0: [1085, 1625, 1100, 1150, 0, 1225],
+                    destination_cost_1: [1000, 1700, 1000, 1100, 0, 1050],
+                    destination_cost_2: [1085, 1625, 1100, 1150, 0, 1225],
+                    destination_cost_3: [1000, 1700, 1000, 1100, 0, 1050]
+                },
+                level_3: {
+                    destination_cost_0: [1085, 1625, 1100, 1150, 0, 1225],
+                    destination_cost_1: [1000, 1700, 1000, 1100, 0, 1050],
+                    destination_cost_2: [1085, 1625, 1100, 1150, 0, 1225],
+                    destination_cost_3: [1000, 1700, 1000, 1100, 0, 1050]
+                },
+                level_4: {
+                    destination_cost_0: [1600, 2200, 1625, 1725, 0, 1775],
+                    destination_cost_1: [1575, 2200, 1575, 1675, 0, 1675],
+                    destination_cost_2: [1600, 2200, 1625, 1725, 0, 1775],
+                    destination_cost_3: [1575, 2200, 1575, 1675, 0, 1675]
+                }
+            }
+        },
+        CANADA: {
+            w_1: {
+                level_1: {
+                    destination_cost_0: [1650],
+                    destination_cost_1: [1650],
+                    destination_cost_2: [1650],
+                    destination_cost_3: [1650]
+                },
+                level_2: {
+                    destination_cost_0: [1650],
+                    destination_cost_1: [1650],
+                    destination_cost_2: [1650],
+                    destination_cost_3: [1650]
+                },
+                level_3: {
+                    destination_cost_0: [1650],
+                    destination_cost_1: [1650],
+                    destination_cost_2: [1650],
+                    destination_cost_3: [1650]
+                },
+                level_4: {
+                    destination_cost_0: [2100],
+                    destination_cost_1: [2100],
+                    destination_cost_2: [2100],
+                    destination_cost_3: [2100]
+                }
+            }
+        },
+        EUROPE: {
+            w_1: {
+                level_1: {
+                    destination_cost_0: [150],
+                    destination_cost_1: [150],
+                    destination_cost_2: [150],
+                    destination_cost_3: [150]
+                },
+                level_2: {
+                    destination_cost_0: [50],
+                    destination_cost_1: [50],
+                    destination_cost_2: [50],
+                    destination_cost_3: [0]
+                },
+                level_3: {
+                    destination_cost_0: [0],
+                    destination_cost_1: [0],
+                    destination_cost_2: [0],
+                    destination_cost_3: [0]
+                },
+                level_4: {
+                    destination_cost_0: [200],
+                    destination_cost_1: [200],
+                    destination_cost_2: [200],
+                    destination_cost_3: [200]
+                }
+            }
+        }
+    }
+};
