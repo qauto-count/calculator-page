259dcbb23fe470884ac25983006da985a56bd964
qauto-count <127936744+qauto-count@users.noreply.github.com>
2023-11-02 20:56:06 +0200

    Add files via upload

--- js_data/usa.js
+++ js_data/usa.js
@@ -312,7 +312,8 @@ let USA = {
                     value_84: ['VALLEJO (CA) 94590**', 400],
                     value_85: ['VAN NUYS (CA) 91405', 180],
                     value_86: ['VAN NUYS (CA) 91405**', 270]
-
+                    value_87: ['Los Angeles South (CA) 90744', 140],
+                    value_88: ['Los Angeles South (CA) 90744**', 220]
                  },
                 NJ: {
                     value_0: ['ALBANY 12205', 250],
